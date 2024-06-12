0f8b92a80503d6fa7f524d409896f4ef4e279367
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-06-11 19:29:00 -0600

    added rock paper scissors and printed for testing

--- vinny.py
+++ vinny.py
@@ -1,1 +1,39 @@
 print("Rock paper scissors game in Python")
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+# is determined by 3 simple rules :
+
+#1. Rock wins against scissors
+#2. Scissors win against paper
+#3. Paper wins against rock
+
+print(paper)
+
