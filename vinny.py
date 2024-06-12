58a121ee9a8f9e11948f5fda758c97363826e1a7
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-06-11 19:35:23 -0600

    just added random function to generate random values

--- vinny.py
+++ vinny.py
@@ -35,5 +35,7 @@ scissors = '''
 #2. Scissors win against paper
 #3. Paper wins against rock
 
-print(paper)
 
+user_chioce = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+computer_choice = random.randint(0, 2)
+print(f"computer chose {computer_choice}")
