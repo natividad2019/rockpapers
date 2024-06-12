9a394ae89f747facb8c4f4781568e2308503b3af
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-06-11 19:47:51 -0600

    added more if statements to do the logic

--- vinny.py
+++ vinny.py
@@ -39,9 +39,12 @@ scissors = '''
 user_chioce = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
 computer_choice = random.randint(0, 2)
 print(f"computer chose {computer_choice}")
+
 if user_chioce ==  0 and computer_choice == 2:
   print("User wins")
 elif computer_choice > user_chioce:
   print("you lose")
-else: 
+elif computer_choice == user_chioce: 
+  print("Its a draw")
+else:
   print("You typed an invalid number, you lose!")
