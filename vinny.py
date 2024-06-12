91e946a17d46e913f3afb3cb6f5c1431c5ab6725
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-06-11 20:04:55 -0600

    add all the elif statements tofinish the project

--- vinny.py
+++ vinny.py
@@ -1,6 +1,6 @@
+import random
 print("Rock paper scissors game in Python")
 
-import random
 
 rock = '''
     _______
@@ -31,20 +31,31 @@ scissors = '''
 
 # is determined by 3 simple rules :
 
-#1. Rock wins against scissors
-#2. Scissors win against paper
-#3. Paper wins against rock
+# 1. Rock wins against scissors
+# 2. Scissors win against paper
+# 3. Paper wins against rock
 
 
-user_chioce = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+game_images = [rock, paper, scissors]
+user_chioce = int(
+    input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_chioce])
 computer_choice = random.randint(0, 2)
-print(f"computer chose {computer_choice}")
+print("computer chose:")
+print(game_images[computer_choice])
+#print(f"computer chose {computer_choice}")
 
-if user_chioce ==  0 and computer_choice == 2:
-  print("User wins")
+if user_chioce >= 3 or user_chioce < 0:
+  print("you typed an invalid number, you lose!")
+
+elif user_chioce == 0 and computer_choice == 2:
+    print("you win!")
+elif computer_choice == 0 and user_chioce == 2:
+    print("you lose")
 elif computer_choice > user_chioce:
-  print("you lose")
-elif computer_choice == user_chioce: 
-  print("Its a draw")
-else:
-  print("You typed an invalid number, you lose!")
+    print("you lose")
+elif user_chioce > computer_choice:
+    print("you win!")
+elif computer_choice == user_chioce:
+    print("Its a draw")
+
