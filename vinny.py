c38fd51d8511c1944566f5f8cdcd0704585a6540
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-06-11 19:41:36 -0600

    added more if statements to check the if user or computer wins

--- vinny.py
+++ vinny.py
@@ -36,6 +36,12 @@ scissors = '''
 #3. Paper wins against rock
 
 
-user_chioce = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+user_chioce = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
 computer_choice = random.randint(0, 2)
 print(f"computer chose {computer_choice}")
+if user_chioce ==  0 and computer_choice == 2:
+  print("User wins")
+elif computer_choice > user_chioce:
+  print("you lose")
+else: 
+  print("You typed an invalid number, you lose!")
